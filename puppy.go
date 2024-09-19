a99e9018bb77cfba31db8d4cc0f53a0de3771437
abudy.gold <abudy.gold@yahoo.com>
2024-09-19 10:36:40 +0700

    add new methow

--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "strings"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func WhenGrownUp(s string) string {
+	return "When the puppy grows up it says: " + strings.ToUpper(s)
 }
